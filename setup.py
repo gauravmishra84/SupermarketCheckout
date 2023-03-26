1e2e4e015d78dc1a6d5d97f84a60b307f0bed84c
gmishraji <gauravmishra84@outlook.com>
2023-03-26 02:24:25 +0100

    Adding Packaging configurations

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='supermarketcheckout',
+    version='0.0.1',
+    install_requires=[
+        'importlib-metadata; python_version > "3.10"',
+    ],
+)
